d832e51e6d74810aa91b9a8799716dbf37975cc9
Administrator <fischer@valkyteq.com>
2023-10-25 03:59:11 +0200

    Moved auth methods in an own Auth class

---
+++ Twitch/auth.py
@@ -0,0 +1,107 @@
+import json
+import http.server
+import socketserver
+import webbrowser
+
+import requests
+
+CODE = None
+
+
+class OAuthCallbackHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        # Extract the OAuth code from the query parameters
+        global CODE
+        try:
+            CODE = self.path.split('?code=')[1].split("&scope")[0]
+        except IndexError:
+            pass
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b'OAuth code received. You can now close this page.')
+        
+
+class Auth:
+    def __init__(self, config: dict, logger):
+        self.config = config
+        self.logger = logger
+        
+    def authorize(self, client_id: str, client_secret: str, redirect_uri: str, scopes: list):
+        """
+        Authorizes the bot to use the Twitch API.
+        
+        Args:
+            client_id (str): The client id of the bot.
+            client_secret (str): The client secret of the bot.
+            redirect_uri (str): The redirect uri of the bot.
+            scopes (list): The scopes of the bot.
+        """
+        self.logger.info('Authorizing Twitch Account')
+        code = self._get_auth_code(client_id, redirect_uri, scopes)
+        data = self._get_auth_token(client_id, client_secret, code, redirect_uri)
+        
+        self.config['twitch']['bot_token'] = data.get('access_token')
+        self.config['twitch']['bot_refresh_token'] = data.get('refresh_token')
+        self.config['twitch']['bot_token_expires'] = data.get('expires_in')
+        
+        with open('settings.json', 'w') as f:
+            json.dump(self.config, f, indent=4)
+        
+        return self.config['twitch']['bot_token']
+        
+    def _get_auth_code(self, client_id: str, redirect_uri: str, scopes: list) -> str:
+        """
+        Gets the OAuth code for the bot. This code will be used to get the OAuth token for the bot.
+        
+        Args:
+            client_id (str): The client id of the bot.
+            redirect_uri (str): The redirect uri of the bot.
+            scopes (list): The scopes of the bot.
+            
+        Returns:
+            str: The OAuth code for the bot.
+        """
+        url = f'https://id.twitch.tv/oauth2/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope={"+".join(scopes)}'
+        # Create a local web server to listen for the response
+        server_address = ('', 8000)  # You can choose a different port if needed
+        httpd = socketserver.TCPServer(server_address, OAuthCallbackHandler)
+        
+        try:
+            # Open the authorization URL in the browser for the user to log in and grant authorization
+            webbrowser.open(url)
+            # Start the web server to listen for the response
+            httpd.handle_request()
+        
+        finally:
+            # Close the web server when done
+            httpd.server_close()
+
+            if CODE is not None:
+                self.logger.info(f'Twitch OAuth code is {CODE}')
+                return CODE
+            else:
+                raise Exception('Failed to get OAuth code')
+        
+    def _get_auth_token(self, client_id: str, client_secret: str, code: str, redirect_uri: str) -> dict:
+        """
+        Gets the OAuth token for the bot.
+        
+        Args:
+            client_id (str): The client id of the bot.
+            client_secret (str): The client secret of the bot.
+            code (str): The OAuth code for the bot.
+            redirect_uri (str): The redirect uri of the bot.
+            
+        Returns:
+            str: The OAuth dict for the bot.
+        """
+        url = f'https://id.twitch.tv/oauth2/token?client_id={client_id}&client_secret={client_secret}&code={code}&grant_type=authorization_code&redirect_uri={redirect_uri}'
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        response = requests.post(url, headers=headers)
+        if response.status_code == 200:
+            response_data = json.loads(response.text)
+            self.logger.info(f'Twitch OAuth token is {response_data.get("access_token")}')
+            return response_data
--- bot_twitch.py
+++ bot_twitch.py
@@ -29,35 +29,17 @@ Example:
 
 """
 
-import json
 import logging
-import http.server
 import random
-import socketserver
-import webbrowser
-import requests
 
 from twitchio.ext import commands, pubsub
+
+from Twitch.auth import Auth
 from Twitch.channel import Channel
 from Twitch.stream import Stream
 from luna import Luna
 
 LUNA: Luna = None
-CODE = None
-
-
-class OAuthCallbackHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        # Extract the OAuth code from the query parameters
-        global CODE
-        try:
-            CODE = self.path.split('?code=')[1].split("&scope")[0]
-        except IndexError:
-            pass
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write(b'OAuth code received. You can now close this page.')
 
 
 class TwitchBot(commands.Bot):
@@ -74,6 +56,7 @@ class TwitchBot(commands.Bot):
         self.config = config
         self.stream = Stream()
         self.channel = Channel(self.config, self.logger, self.stream)
+        self.auth = Auth(self.config, self.logger)
         prefix = self.config['twitch']['prefix']
         scopes = [
             "analytics:read:extensions",
@@ -141,7 +124,7 @@ class TwitchBot(commands.Bot):
         ]
         channels = [f"#{self.channel.name}"]
         if self.config['twitch']['bot_token'] is None or self.config['twitch']['bot_token'] == "":
-            bot_token = self.authorize(self.config['twitch']['client_id'], self.config['twitch']['client_secret'], self.config['twitch']['redirect_uri'], scopes)
+            bot_token = self.auth.authorize(self.config['twitch']['client_id'], self.config['twitch']['client_secret'], self.config['twitch']['redirect_uri'], scopes)
         else:
             # TODO: Add a check to see if the bot token is valid
             bot_token = self.config['twitch']['bot_token']
@@ -244,85 +227,6 @@ class TwitchBot(commands.Bot):
         # user_id = await self.channel.get_id(user_name)
         is_live = await self.channel.get_status(self.channel.id)
         return is_live
-        
-    def authorize(self, client_id: str, client_secret: str, redirect_uri: str, scopes: list):
-        """
-        Authorizes the bot to use the Twitch API.
-        
-        Args:
-            client_id (str): The client id of the bot.
-            client_secret (str): The client secret of the bot.
-            redirect_uri (str): The redirect uri of the bot.
-            scopes (list): The scopes of the bot.
-        """
-        self.logger.info('Authorizing Twitch Account')
-        code = self._get_auth_code(client_id, redirect_uri, scopes)
-        data = self._get_auth_token(client_id, client_secret, code, redirect_uri)
-        
-        self.config['twitch']['bot_token'] = data.get('access_token')
-        self.config['twitch']['bot_refresh_token'] = data.get('refresh_token')
-        self.config['twitch']['bot_token_expires'] = data.get('expires_in')
-        
-        with open('settings.json', 'w') as f:
-            json.dump(self.config, f, indent=4)
-        
-        return self.config['twitch']['bot_token']
-        
-    def _get_auth_code(self, client_id: str, redirect_uri: str, scopes: list) -> str:
-        """
-        Gets the OAuth code for the bot. This code will be used to get the OAuth token for the bot.
-        
-        Args:
-            client_id (str): The client id of the bot.
-            redirect_uri (str): The redirect uri of the bot.
-            scopes (list): The scopes of the bot.
-            
-        Returns:
-            str: The OAuth code for the bot.
-        """
-        url = f'https://id.twitch.tv/oauth2/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope={"+".join(scopes)}'
-        # Create a local web server to listen for the response
-        server_address = ('', 8000)  # You can choose a different port if needed
-        httpd = socketserver.TCPServer(server_address, OAuthCallbackHandler)
-        
-        try:
-            # Open the authorization URL in the browser for the user to log in and grant authorization
-            webbrowser.open(url)
-            # Start the web server to listen for the response
-            httpd.handle_request()
-        
-        finally:
-            # Close the web server when done
-            httpd.server_close()
-
-            if CODE is not None:
-                self.logger.info(f'Twitch OAuth code is {CODE}')
-                return CODE
-            else:
-                raise Exception('Failed to get OAuth code')
-        
-    def _get_auth_token(self, client_id: str, client_secret: str, code: str, redirect_uri: str) -> dict:
-        """
-        Gets the OAuth token for the bot.
-        
-        Args:
-            client_id (str): The client id of the bot.
-            client_secret (str): The client secret of the bot.
-            code (str): The OAuth code for the bot.
-            redirect_uri (str): The redirect uri of the bot.
-            
-        Returns:
-            str: The OAuth dict for the bot.
-        """
-        url = f'https://id.twitch.tv/oauth2/token?client_id={client_id}&client_secret={client_secret}&code={code}&grant_type=authorization_code&redirect_uri={redirect_uri}'
-        headers = {
-            'Content-Type': 'application/json'
-        }
-        response = requests.post(url, headers=headers)
-        if response.status_code == 200:
-            response_data = json.loads(response.text)
-            self.logger.info(f'Twitch OAuth token is {response_data.get("access_token")}')
-            return response_data
 
     # ==================================================================================================================
     # Commands
